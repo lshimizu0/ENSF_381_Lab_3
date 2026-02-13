62346a2595c2a284656b2bb0d6beee3334a04a23
kevincharlton11 <144822134+kevincharlton11@users.noreply.github.com>
2026-02-13 12:22:30 -0700

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,26 @@
+
+let classRoster = ["Alice", "Tom", "Charlie", "Diana", "Evan"]
+
+console.log(classRoster)
+
+console.log(classRoster.toString())
+
+classRoster.push("Fiona", "Nancy")
+
+console.log(classRoster.shift())
+
+console.log(classRoster)
+
+console.log(classRoster.length)
+
+
+
+const classInfo = {
+    className: "ENSF381: Full-Stack Web Development",
+    instructor: "Dr. Smith",
+    students: classRoster,
+    details: {
+        semester: "Winter",
+        year: "2025"
+    }
+}
